203475ac8dfa217bb00bb2b80d7cd07631b50271
Matheus Gois <matheusgoislimasilva@gmail.com>
2019-07-20 07:17:08 -0300

    go

--- server.js
+++ server.js
@@ -34,6 +34,6 @@ io.on("connection", function (client) {
 });
 
 
-http.listen(3000, function(){
-  console.log('listening on port 3000');
+http.listen(process.env.PORT || 3000, function(){
+  console.log('listening...');
 });
