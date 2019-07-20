baa4b192a871e68e74d0bea4965b75fd6cb57a36
Matheus Gois <matheusgoislimasilva@gmail.com>
2019-07-20 02:11:04 -0300

    first

---
+++ Chat-App/Chat-App/Mensage.swift
@@ -0,0 +1,12 @@
+//
+//  Mensage.swift
+//  Chat-App
+//
+//  Created by Matheus Gois on 20/07/19.
+//  Copyright © 2019 Matheus Gois. All rights reserved.
+//
+
+struct Mensage {
+    var user:String
+    var msg:String
+}
